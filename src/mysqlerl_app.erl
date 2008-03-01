9d698172ae5b68953ed899a0743e65f601cd37c7
Brian Cully <bjc@kublai.com>
2008-03-01 14:44:39 -0500

    Prettify the supervision tree w/ names. Break modules out by registered names.

--- src/mysqlerl_app.erl
+++ src/mysqlerl_app.erl
@@ -2,20 +2,12 @@
 -author('bjc@kublai.com').
 
 -behavior(application).
--behavior(supervisor).
 
--export([start/2, stop/1, init/1]).
+-export([start/2, stop/1]).
 
 start(normal, []) ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    register(?MODULE, self()),
+    mysqlerl_sup:start_link().
 
 stop([]) ->
     ok.
-
-init([]) ->
-    ConnectionSup = {mysqlerl_connection_sup,
-                     {mysqlerl_connection_sup, start_link, []},
-                     permanent, infinity, supervisor,
-                     [mysqlerl_connection_sup]},
-    {ok, {{one_for_one, 10, 5},
-          [ConnectionSup]}}.
---
+++ src/mysqlerl_sup.erl
@@ -0,0 +1,17 @@
+-module(mysqlerl_sup).
+-author('bjc@kublai.com').
+
+-behavior(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    ConnectionSup = {mysqlerl_connection_sup,
+                     {mysqlerl_connection_sup, start_link, []},
+                     permanent, infinity, supervisor,
+                     [mysqlerl_connection_sup]},
+    {ok, {{one_for_one, 10, 5},
+          [ConnectionSup]}}.
