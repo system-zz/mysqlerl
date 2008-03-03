ec2a085aee1b22db14f3383075abfc7fe35f80f6
Brian Cully <github.20.shmit@spamgourmet.com>
2008-03-02 19:48:20 -0500

    Remove query from select.

--- src/mysqlerl.hrl
+++ src/mysqlerl.hrl
@@ -1,7 +1,7 @@
 -record(sql_query, {q}).
 -record(sql_param_query, {q, params}).
 -record(sql_select_count, {q}).
--record(sql_select, {q, pos, n}).
+-record(sql_select, {pos, n}).
 -record(sql_first, {}).
 -record(sql_last, {}).
 -record(sql_next, {}).
